102b9d25a74e423111ee4ed4bbdc48329eb7ac50
mxz <836323493@qq.com>
2018-05-10 02:06:03 +0800

    two

---
+++ mxz-security-app/src/main/java/com/mxz/security/app/MxzResourceServerConfig.java
@@ -0,0 +1,16 @@
+package com.mxz.security.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/*作者：马兴争
+ *日期: 2018年5月9日
+ *时间： 上午1:22:09
+ **/
+@Configuration
+@EnableResourceServer
+public class MxzResourceServerConfig {
+
+}
+ 
